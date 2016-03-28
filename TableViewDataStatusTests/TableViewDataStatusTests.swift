77b1c31405e0f73a92ce74ce3b29cd3d14874c44
Gabriel Massana <gabrielmassana@gmail.com>
2016-03-28 22:31:37 +0100

    testing init

--- TableViewDataStatusTests/TableViewDataStatusTests.swift
+++ TableViewDataStatusTests/TableViewDataStatusTests.swift
@@ -8,13 +8,17 @@
 
 import XCTest
 
+@testable import TableViewDataStatus
+
 class TableViewDataStatusTests: XCTestCase {
+    
+    var tableView: TableViewDataStatus?
     
     override func setUp() {
         
         super.setUp()
         
-
+        tableView = TableViewDataStatus.init(frame: CGRectZero, style: .Plain)
     }
     
     override func tearDown() {
@@ -24,5 +28,13 @@ class TableViewDataStatusTests: XCTestCase {
     }
     
 
-    
+    func test () {
+        
+        // https://www.raywenderlich.com/101306/unit-testing-tutorial-mocking-objects
+        
+        // http://blog.eliperkins.me/mocks-in-swift-via-protocols
+        // http://masilotti.com/better-swift-unit-testing/
+        // http://clean-swift.com/to-mock-or-not-to-mock/
+        // https://christiantietze.de/posts/2015/06/mocks-swift/
+    }
 }
