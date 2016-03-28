cc20efa5604a9570a90e04b5a893ab7f17f16cbc
Gabriel Massana <gabrielmassana@gmail.com>
2016-03-28 21:21:36 +0100

    Added target test

---
+++ TableViewDataStatusTests/TableViewDataStatusTests.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewDataStatusTests.swift
+//  TableViewDataStatusTests
+//
+//  Created by Gabriel Massana on 28/3/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+import XCTest
+
+class TableViewDataStatusTests: XCTestCase {
+    
+    override func setUp() {
+        
+        super.setUp()
+        
+
+    }
+    
+    override func tearDown() {
+
+        
+        super.tearDown()
+    }
+    
+
+    
+}
